4c440705154479387f260d00faee66c37cfc83bd
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-10-15 03:46:14 +0700

    add <Helmet> tag and copy meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ThemeProviders from "./helpers/theme-providers.jsx";
 import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
 import { Toaster } from "react-hot-toast";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
@@ -14,6 +14,29 @@ createRoot(document.getElementById("root")).render(
 			<Header />
 			<Toaster />
 			<HelmetProvider>
+				<Helmet>
+					<meta property="og:title" content="React Catatan Pribadi" />
+					<meta
+						property="og:description"
+						content="Aplikasi catatan pribadi oleh
+		Hydare."
+					/>
+					<meta property="og:site_name" content="React Catatan Pribadi" />
+					<meta
+						property="og:url"
+						content="https://react-notes-hydare.vercel.app"
+					/>
+					<meta
+						property="og:image"
+						content="https://raw.githubusercontent.com/naufalHaidar12342/react-notes-hydare/refs/heads/main/public/calum-macaulay-CxYHfBkC0vs-unsplash.jpg"
+					/>
+					<meta property="og:image:width" content="1200" />
+					<meta property="og:image:height" content="630" />
+					<meta
+						property="og:image:alt"
+						content="Filling a blank page. Photo by Calum MacAulay(https://unsplash.com/@calum_mac?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash) on Unsplash (https://unsplash.com/photos/person-writing-on-notepad-CxYHfBkC0vs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash)"
+					/>
+				</Helmet>
 				<App />
 			</HelmetProvider>
 			<Footer />
