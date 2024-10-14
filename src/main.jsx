2d6c83a4cf9316eb771e3397b1eb696e4f240b0a
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-10-15 03:39:23 +0700

    add react-helmet-async and HelmetProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import ThemeProviders from "./helpers/theme-providers.jsx";
 import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
 import { Toaster } from "react-hot-toast";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<ThemeProviders>
 			<Header />
 			<Toaster />
-			<App />
+			<HelmetProvider>
+				<App />
+			</HelmetProvider>
 			<Footer />
 		</ThemeProviders>
 	</StrictMode>
