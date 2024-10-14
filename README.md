3d1e8318de7ea6a954cf858518b1cb9c17867fd5
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-10-14 18:11:04 +0700

    edit content of README in root directory, add state for requested notes title

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-# React + Vite
+# react-notes-hydare
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A personal notes application developed with Vite + React (Vite with React JavaScript template), Tailwind CSS, NextUI.
 
-Currently, two official plugins are available:
+This application was developed to fulfill the submission/assignment for the Dicoding class "Belajar Membuat Aplikasi Web dengan React".
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Running the project
+
+In the Visual Studio Code terminal, please type and run the command `npm run dev`
+
+## Fitur aplikasi
+
+So far, these features are available to try:
+
+- add new notes (mandatory for submission)
+- delete notes (mandatory for submission)
+- show initial data
+- move note from active notes to archive notes and vice versa
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import NotesForm from "./components/notesForm.jsx";
 import ActiveNotesBody from "./components/active-notes-section/activeNotesBody.jsx";
 import ArchiveNotesBody from "./components/archive-notes-section/archiveNotesBody.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import getInitialNotes from "./utilities/initial-notes.js";
 import TimestampGenerator from "./utilities/timestamp-generator.js";
 import TimestampISOFormatGenerator from "./utilities/timestamp-isoformat-generator.js";
@@ -9,6 +9,11 @@ import SearchNotesForm from "./components/searchNotesForm.jsx";
 
 export default function App() {
 	const [notes, setNotes] = useState(getInitialNotes());
+	const [notesTitleSearched, setNotesTitleSearched] = useState("");
+
+	const searchedNotes = notes.filter((note) =>
+		note.title.toLowerCase().includes(notesTitleSearched.toLowerCase())
+	);
 
 	const onActivateNote = (notesId) => {
 		const updatedNotes = notes.map((note) => {
@@ -46,15 +51,9 @@ export default function App() {
 		setNotes([...notes, newNote]);
 	};
 
-	const onSearchNotesHandler = (notesTitle, previousState) => {
-		console.log("notesTitle=", notesTitle);
-		const searchedNotes = notes.filter((note) =>
-			note.title.toLowerCase().includes(notesTitle.toLowerCase())
-		);
-		if (searchedNotes.length === 0) {
-			setNotes([...previousState]);
-		}
-		setNotes(searchedNotes);
+	const onSearchNotesHandler = (notesTitle) => {
+		// console.log("notesTitle=", notesTitle);
+		setNotesTitleSearched(notesTitle);
 	};
 	return (
 		<div className="flex flex-col py-10 gap-10">
